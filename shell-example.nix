261c1c064b32f01dfc58eb82bfa2dc14250d2c31
bib0x <bib0x.github@bootsect.org>
2022-03-15 22:32:35 +0100

    add example nix-shell

--- shell-example.nix
+++ shell-example.nix
@@ -2,6 +2,6 @@ with (import <nixpkgs> {});
 mkShell {
   buildInputs = [ gandi-cli ];
   shellHook = ''
-    export API_KEY=$(api-store <key>)
+    export API_KEY=$(PASSWORD_STORE_DIR=<dir> pass <key>)
   '';
 }
